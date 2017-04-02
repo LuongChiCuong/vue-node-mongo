000522d1d4e76f5a8e3091d0d1f7de4e3aac43c3
LuongChiCuong <luongchicuong93@gmail.com>
2017-04-02 16:37:18 +0700

    add node production

---
+++ node-server/config/env/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Development configuration options
+  // db: 'mongodb://localhost/mean',
+  db: 'mongodb://admin:cuong97541@ds137110.mlab.com:37110/nvm',
+  sessionSecret: 'productionSessionSecret'
+};
